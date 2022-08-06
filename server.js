1f92b1e16e7a8d0aa697865a7f54fe3cbb2739e8
tarunkethwalia <tarunkethwalia18@gmail.com>
2022-08-06 18:45:52 +0530

    port changed

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const app = require('./app');
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
